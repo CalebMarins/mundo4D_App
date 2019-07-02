604045846c404ac9e972318a4976e2fefddfa365
Caleb Marins <caleb.marins@gmail.com>
2019-07-02 05:50:45 -0300

    Design telas login_cadastro_esqueceu

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,15 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  @ViewChild(IonSlides) slides : IonSlides;
 
+  //Divisão de telas
   splash= true;
+  login= true;
+  esqueceu= false;
 
   constructor() { }
 
   
   ngOnInit() {
+    //Definindo tempo de splash screen
     setTimeout(()=>{
       this.splash = false;
     }, 4000);
-  }  
+  } 
+  // Definindo evento de clique no segmento
+  segmentChanged(event : any){
+    if (event.detail.value ==="login"){
+      this.slides.slidePrev();
+    }
+    else{
+      this.slides.slideNext();
+    }
+  }
+  // Mudança de tela pra Esqueceu sua senha
+  exibirEsqueceu() {
+    this.login = false;
+    this.esqueceu = true;
+  }
+  // Mudança de tela pro Login
+  exibirLogin() {
+    this.login = true;
+    this.esqueceu = false;
+  }
+
 }
